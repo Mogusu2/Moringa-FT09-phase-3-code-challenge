344c46acaf24b9d75355e90c487e4efca23185fd
James Maroko <jamiemaroko41@gmail.com>
2024-12-15 23:27:51 +0300

    Updated magazine.py

--- models/magazine.py
+++ models/magazine.py
@@ -1,34 +1,23 @@
 from database.connection import get_db_connection
 
 class Magazine:
-    def __init__(self, id, name, category):
-        self.id = id
-        self.name = name
-        self.category = category
-
     @staticmethod
     def create(name, category):
         conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO magazines (name, category) VALUES (?, ?)', (name, category))
-        conn.commit()
-        conn.close()
-        print(f"Magazine '{name}' created successfully!")
+        try:
+            cursor.execute('INSERT INTO magazines (name, category) VALUES (?, ?)', (name, category))
+            conn.commit()
+            return cursor.lastrowid
+        finally:
+            conn.close()
 
     @staticmethod
-    def read_all():
+    def get_all():
         conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM magazines')
-        rows = cursor.fetchall()
-        conn.close()
-        return [Magazine(row['id'], row['name'], row['category']) for row in rows]
-
-    @staticmethod
-    def get_magazine_by_id(magazine_id):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM magazines WHERE id = ?', (magazine_id,))
-        row = cursor.fetchone()
-        conn.close()
-        return Magazine(row['id'], row['name'], row['category']) if row else None
+        try:
+            cursor.execute('SELECT * FROM magazines')
+            return cursor.fetchall()
+        finally:
+            conn.close()
