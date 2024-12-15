cf31f34f6e08fdc20e1fac8b91bc9a5e41fa3eff
James Maroko <jamiemaroko41@gmail.com>
2024-12-15 23:27:15 +0300

    Updated author.py

--- models/author.py
+++ models/author.py
@@ -1,33 +1,23 @@
 from database.connection import get_db_connection
 
 class Author:
-    def __init__(self, id, name):
-        self.id = id
-        self.name = name
-
     @staticmethod
     def create(name):
         conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO authors (name) VALUES (?)', (name,))
-        conn.commit()
-        conn.close()
-        print(f"Author '{name}' created successfully!")
+        try:
+            cursor.execute('INSERT INTO authors (name) VALUES (?)', (name,))
+            conn.commit()
+            return cursor.lastrowid
+        finally:
+            conn.close()
 
     @staticmethod
-    def read_all():
+    def get_all():
         conn = get_db_connection()
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM authors')
-        rows = cursor.fetchall()
-        conn.close()
-        return [Author(row['id'], row['name']) for row in rows]
-
-    @staticmethod
-    def get_author_by_id(author_id):
-        conn = get_db_connection()
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM authors WHERE id = ?', (author_id,))
-        row = cursor.fetchone()
-        conn.close()
-        return Author(row['id'], row['name']) if row else None
+        try:
+            cursor.execute('SELECT * FROM authors')
+            return cursor.fetchall()
+        finally:
+            conn.close()
